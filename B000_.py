4e8c022ea2f04744d156eea7c82ad7222e9eaff7
Mahdi <imahdimir@gmail.com>
2025-04-01 17:25:44 -0700

    update

--- B000_.py
+++ B000_.py
@@ -1,17 +1,16 @@
-#!/homes/nber/mahdimir/bulk/.pyenv/versions/3.12.2/envs/250331_CSF_SSGAC_Alex_sibsum_sibdiff_analysis_using_array_data/bin/python3
+#!/homes/nber/mahdimir/bulk/.pyenv/versions/3.12.2/envs/250331_SSGAC_Alex_UKB_sibsum_sibdiff_analysis_using_array_data/bin/python3
 
 """
 
 
 """
-
-from pathlib import Path
-
+import os
 import pandas as pd
+from pathlib import Path
 
 
 class Directory :
-    data_dir = '/var/genetics/ws/mahdimir/projects_data/25Q1/250331_CSF_SSGAC_Alex_sibsum_sibdiff_analysis_using_array_data'
+    data_dir = '/disk/genetics/ukb/mahdimir/UKB_PROJECTS_DATA/25Q1/250331_CSF_SSGAC_Alex_UKB_sibsum_sibdiff_analysis_using_array_data'
     data_dir = Path(data_dir)
     dta = data_dir
 
@@ -19,11 +18,23 @@ class Directory :
     med = dta / 'med'
     out = dta / 'out'
 
+    ukb_genotype_dir = Path('/disk/genetics2/ukb/orig/UKBv3/genotyped_data')
+
 
 class FilePath :
     d = Directory()
 
-    chr22 = '/disk/genetics2/ukb/orig/UKBv3/genotyped_data/ukb_cal_chr22_v2.bed.gz'
+    cal_chr22_bed_gz_src = d.ukb_genotype_dir /'ukb_cal_chr22_v2.bed.gz'
+    cal_chr22_bed_gz = d.inp / 'ukb_cal_chr22_v2.bed.gz'
+
+    ukb_snp_chr22_bim_gz = d.ukb_genotype_dir / 'ukb_snp_chr22_v2.bim.gz'
+
+
+    cal_chr22_bed = d.inp / 'ukb_cal_chr22_v2.bed'
+
+    hap_fam_22_src = "/disk/genetics4/ukb/alextisyoung/haplotypes/ukb_hap_chr22_v2.fam"
+    hap_fam_22 = d.inp / 'ukb_hap_chr22_v2.fam'
+    cal_chr22_fam = d.inp / 'ukb_cal_chr22_v2.fam'
 
 
 class FilePathPattern :
@@ -38,43 +49,68 @@ def main() :
     pass
 
     ##
+    d = Directory()
     fp = FilePath()
 
     ##
-    import gzip
-    import pandas as pd
+    def copy_input_files_into_project_data_folder() :
+        """
+        Copy input files into the project data folder.
+        """
+        pass
 
+        ##
+        src_file__dest_dir = {
+                fp.cal_chr22_bed_gz_src : d.inp ,
+                fp.hap_fam_22_src       : d.inp ,
 
-    # Open the gzip file
-    with gzip.open(fp.chr22 , 'rt') as f :
-        # Read the file into a pandas DataFrame
-        df = pd.read_csv(f , sep = '\t' , header = None)
+                }
 
-    # Display the first few rows of the DataFrame
-    print(df.head())
-
-    ##
-    import gzip
-
-
-    # Open the gzip file in binary mode
-    with gzip.open(fp.chr22 , 'rb') as f :
-        # Read the contents of the file
-        file_content = f.read()
-
-    # Process the binary data as needed
-    # For example, you can print the first few bytes
-    print(file_content[:100])
-
+        for src , dest in src_file__dest_dir.items() :
+            # Copy the file to the destination directory
+            shutil.copy(src , dest_dir)
+            print(f"Copied {src} to {dest_dir}")
 
 
     ##
+    # Rename the file
+    orig_file = fp.hap_fam_22
+    renamed_file = fp.cal_chr22_fam
 
+    os.rename(orig_file , renamed_file)
+
+    print(f"File has been renamed from {orig_file} to {renamed_file}")
 
     ##
 
 
+
+
     ##
+    bed_df = pd.read_csv(fp.cal_chr22_bed , sep = '\t' , header = None)
+
+    # Display the first few rows of the dataframe
+    print(bed_df.head())
+
+    ##
+    import numpy as np
+    from bed_reader import open_bed , sample_file
+
+
+    bed = open_bed(fp.cal_chr22_bed, bim_location = fp.ukb_snp_chr22_bim_gz, fam_location = fp.hap_fam_22_src)
+    val = bed.read()
+    print(val)
+
+    ##
+    [[1.  0. nan  0.][2.  0. nan  2.][0.
+    1.
+    2.
+    0.]] >> > del bed
+
+
+    ##
+
+
 
 
 

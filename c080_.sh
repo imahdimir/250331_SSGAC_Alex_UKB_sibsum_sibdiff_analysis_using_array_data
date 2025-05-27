391abe4ede2aea60186a7811e452d400bce0c659
Mahdi <imahdimir@gmail.com>
2025-05-26 17:16:19 -0700

    Update c080_.sh

--- c080_.sh
+++ c080_.sh
@@ -1,6 +1,10 @@
-bed_fp="/disk/genetics/ukb/mahdimir/UKB_NON_PROJECT_DATA/i24_n1_SSGAC_OnlyOnNBERserver_MahdiUKBws_UKB_NON_PROJECT_DATA_UKBv3_raw_genotyped_decompressed/ukb_cal_chr22_v2.bed"
-bim_fp="/disk/genetics/ukb/mahdimir/UKB_NON_PROJECT_DATA/i24_n1_SSGAC_OnlyOnNBERserver_MahdiUKBws_UKB_NON_PROJECT_DATA_UKBv3_raw_genotyped_decompressed/ukb_snp_chr22_v2.bim"
-fam_fp="/disk/genetics/ukb/mahdimir/UKB_PROJECTS_DATA/i18/i18_n1_SSGAC_tA1_Alex_UKB_sibsum_sibdiff_analysis_using_array_genotyped_data/med/fam_with_ped.fam"
+#!/bin/bash
+
+decomp_arr_dta_dir="/disk/genetics/ukb/mahdimir/UKB_NON_PROJECT_DATA/decompressed__UKBv3_raw_genotyped"
+
+bed_fp="$decomp_arr_dta_dir/ukb_cal_chr22_v2.bed"
+bim_fp="$decomp_arr_dta_dir/ukb_snp_chr22_v2.bim"
+fam_fp="$decomp_arr_dta_dir/med/fam_with_ped.fam"
 
 cd /disk/genetics/ukb/mahdimir/UKB_PROJECTS_DATA/i18/i18_n1_SSGAC_tA1_Alex_UKB_sibsum_sibdiff_analysis_using_array_genotyped_data/out/mendelian_error
 
